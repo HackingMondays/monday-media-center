35c64438ef95ad1e879fb5711e548bca4d48f0f1
Rodrigo <reyes.rr@gmail.com>
2014-08-31 17:00:09 +0200

    Traps Node's uncaught exceptions, and trace them in the console (replaces the nodewebkit default behavior that bogusly replaces the current page to display the stack trace)

--- backend/bootstrap.js
+++ backend/bootstrap.js
@@ -5,3 +5,5 @@ exports.installmmc = function () {
     window.mmc = require('./services');
     console.log("mmc global object installed.");
 };
+
+process.on("uncaughtException", function(err) { console.error("UNCAUGHT EXCEPTION ", err); });
