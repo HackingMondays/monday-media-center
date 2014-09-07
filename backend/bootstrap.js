2e5b1b2bb41f1035517cda6f2fa44499f1305be5
Rodrigo <reyesrr@gmail.com>
2014-09-07 09:34:31 +0200

    Adds ES6 support to Node using Traceur

--- backend/bootstrap.js
+++ backend/bootstrap.js
@@ -1,9 +1,16 @@
+process.on("uncaughtException", function(err) {
+    console.log("Uncaught Exception in Node context: " + err.message);
+    process.stdout.write(err.stack + "\n");
+});
+
 // Register CoffeeScript require wrapper
 require('coffee-script/register');
 
+var traceur = require("traceur");
+traceur.require.makeDefault(function(filename) {
+    return filename.endsWith(".js") && filename.indexOf('node_modules')==-1;
+});
+
 exports.installmmc = function () {
     window.mmc = require('./services');
-    console.log("mmc global object installed.");
 };
-
-process.on("uncaughtException", function(err) { console.error("UNCAUGHT EXCEPTION ", err); });
